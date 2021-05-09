4228c9f891915ea15b6c6d997c3934c4f07c0bdc
Nahir Arrejoria <nahir.arrejoria@ibm.com>
2021-05-09 18:47:42 -0300

    estilos al home

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ app.listen(3000, () =>{
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/shoppingCart.html'));
+});
