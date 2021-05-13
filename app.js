04b58e2a7b7822fc20266332126f558fa5ae5494
PabloV <pablovalentin09@gmail.com>
2021-05-13 02:35:03 -0300

    Se agrega pagina de productos

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.get('/carrito', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
+
 app.get('/registro', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/registro.html'));
+});
+
+app.get('/producto', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/producto.html'));
 });
