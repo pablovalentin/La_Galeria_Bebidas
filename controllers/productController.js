7368227d1435770cb63fc90e980eda694ca8ef58
PabloV <pablovalentin09@gmail.com>
2021-05-29 17:14:06 -0300

    Creadas pgias de alta y modificacion de productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,7 +6,14 @@ const productController = {
         //res.sendFile(path.resolve(viewsPath, './views/producto.ejs'))
         return res.render('producto')
     },
-    
+    nuevoProducto: function(req, res){
+        //res.sendFile(path.resolve(viewsPath, './views/producto.ejs'))
+        return res.render('newProduct')
+    },
+    editarProducto: function(req, res){
+        //res.sendFile(path.resolve(viewsPath, './views/producto.ejs'))
+        return res.render('editProduct')
+    },
 }
 
 module.exports = productController;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,5 +4,7 @@ const productController = require ('../controllers/productController.js')
 
 
 router.get('/product', productController.producto);
+router.get('/newProduct', productController.nuevoProducto);
+router.get('/editProduct', productController.editarProducto);
 
 module.exports = router;
