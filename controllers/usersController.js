4e31694deac0f7ac4ae6f1cd27e099ea8a191199
Nahir Arrejoria <nahir.arrejoria@ibm.com>
2021-06-16 20:59:02 -0300

    se agrupó las vistas de registro y login en una carpeta

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,12 +4,12 @@ const viewsPath = path.join(__dirname, '../')
 const usersController = {
     login: function(req, res){
         //res.sendFile(path.resolve(viewsPath, './views/login.ejs'))
-        return res.render('login')
+        return res.render('users/login')
         
     },
     registro: function(req, res) {
         //res.sendFile(path.resolve(viewsPath, './views/registro.ejs'))
-        return res.render('registro')
+        return res.render('users/registro')
     },
 }
 
