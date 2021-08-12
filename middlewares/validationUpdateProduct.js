9b1d294a3e28c0f6f617397cfecd19eca8369877
nahirar <arrejorianahir@gmail.com>
2021-08-12 09:46:01 -0300

    validacion backed de tablas secundarias para creacion y modificacion de productos

--- middlewares/validationUpdateProduct.js
+++ middlewares/validationUpdateProduct.js
@@ -1,5 +1,6 @@
 const { body } = require('express-validator')
 const { isFileImage } = require('../helpers/file')
+const { Variety, Category} = require ('../database/models')
 
 
 const validationUpdateProduct = [
@@ -15,6 +16,28 @@ const validationUpdateProduct = [
         .bail()
         .isLength({ min: 20 })
         .withMessage('Por favor ingrese una descripción más larga.'),
+    body('categoryId')
+        .custom((categoryId) => {
+            const idCategory = Number(categoryId) ? Number(categoryId) : null
+            return Category.findByPk(idCategory)
+                .then(function (category) {
+                    if(!category){
+                        return Promise.reject('La categoría no es válida')
+                    }
+                })
+                
+        }),
+    body('varietyId')
+        .custom((varietyId) => {
+            const idVariety = Number(varietyId)  ? Number(varietyId) : null 
+            return Variety.findByPk(idVariety)
+                .then(function (variety) {
+                    if(!variety){
+                        return Promise.reject('La variedad no es válida')
+                    }
+                })
+                
+        }),
     body('image')
         .custom((value, { req }) => {
             const { file } = req
