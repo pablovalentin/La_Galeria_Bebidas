2c5799c248a2219fca0492f40543e9d56c4bb010
Marie <marie@Maries-iMac.local>
2021-08-16 21:02:00 -0300

    validacion front ok

--- public/js/form-validation-login.js
+++ public/js/form-validation-login.js
@@ -1,4 +1,5 @@
 console.log("Hola mundo")
+window.onload = function(){
 const form = document.querySelector("#form-login")
 
 // capturamos los imputs, usamos el nombre de la variable declarada para ser mas especificos
@@ -8,33 +9,47 @@ const inputPassword = form.querySelector('#password')
 // capturamos los mensajes de errors
 const errorEmail = form.querySelector('.msg-error-email')
 const errorPassword = form.querySelector('.msg-error-password')
+const msgErrorsArray = [errorEmail, errorPassword]
 
 function validateEmail(inputEmail) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(inputEmail).toLowerCase());    
 }    
 
-form.addEventListener('submit', (e) => {
-    errorEmail.innerHTML = ""
-    errorPassword.innerHTML =""
+function validateForm(e) {
+    console.log("entro al validate")
+    let hasErrors = false
+    resetErrors() 
 
     const email = inputEmail.value
 
     //mail
     if (!validateEmail(email)) {
+        hasErrors = true
         errorEmail.innerHTML = "Por favor ingresá un e-mail correcto"
         inputEmail.focus()
     }
 
     //password
     if (inputPassword.value.length < 3) {
+        hasErrors = true
         errorPassword.innerHTML = "Escriba un password correcto"
         inputPassword.focus()
     }
 
 
-    //bloqueamos la ejecucion por Default
-    e.preventDefault()
+    if (hasErrors) {
+        e.preventDefault()
+        console.log("llego al Default")
+    }
+        
+}
 
-})
+function resetErrors() {
+    msgErrorsArray.forEach(msg => {
+        msg.innerHTML = ""
+    })
+}
 
+form.addEventListener('submit', validateForm)
+}
